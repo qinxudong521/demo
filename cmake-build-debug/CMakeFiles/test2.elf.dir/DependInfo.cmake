34a108d6716fb21186a0cd4e16752f68fce14c58
qxd <750130176@qq.com>
2022-03-05 16:14:32 +0800

    新建托åtest2

---
+++ cmake-build-debug/CMakeFiles/test2.elf.dir/DependInfo.cmake
@@ -0,0 +1,91 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "G:/project/proj_clion/test2/Core/Startup/startup_stm32f429zitx.s" "G:/project/proj_clion/test2/cmake-build-debug/CMakeFiles/test2.elf.dir/Core/Startup/startup_stm32f429zitx.s.obj"
+  "G:/project/proj_clion/test2/Middlewares/Third_Party/RealThread_RTOS/libcpu/arm/cortex-m4/context_gcc.S" "G:/project/proj_clion/test2/cmake-build-debug/CMakeFiles/test2.elf.dir/Middlewares/Third_Party/RealThread_RTOS/libcpu/arm/cortex-m4/context_gcc.S.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32F429xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../RT-Thread"
+  "../Core/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "../Drivers/CMSIS/Include"
+  "../Middlewares/Third_Party/RealThread_RTOS/include"
+  "../Middlewares/Third_Party/RealThread_RTOS/bsp/_template/cubemx_config"
+  "../Middlewares/Third_Party/RealThread_RTOS/finsh"
+  "../Middlewares/Third_Party/RealThread_RTOS/components/finsh"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "G:/project/proj_clion/test2/Core/Src/board.c" "CMakeFiles/test2.elf.dir/Core/Src/board.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Core/Src/board.c.obj.d"
+  "G:/project/proj_clion/test2/Core/Src/main.c" "CMakeFiles/test2.elf.dir/Core/Src/main.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Core/Src/main.c.obj.d"
+  "G:/project/proj_clion/test2/Core/Src/stm32f4xx_hal_msp.c" "CMakeFiles/test2.elf.dir/Core/Src/stm32f4xx_hal_msp.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Core/Src/stm32f4xx_hal_msp.c.obj.d"
+  "G:/project/proj_clion/test2/Core/Src/stm32f4xx_hal_timebase_tim.c" "CMakeFiles/test2.elf.dir/Core/Src/stm32f4xx_hal_timebase_tim.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Core/Src/stm32f4xx_hal_timebase_tim.c.obj.d"
+  "G:/project/proj_clion/test2/Core/Src/stm32f4xx_it.c" "CMakeFiles/test2.elf.dir/Core/Src/stm32f4xx_it.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Core/Src/stm32f4xx_it.c.obj.d"
+  "G:/project/proj_clion/test2/Core/Src/syscalls.c" "CMakeFiles/test2.elf.dir/Core/Src/syscalls.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Core/Src/syscalls.c.obj.d"
+  "G:/project/proj_clion/test2/Core/Src/sysmem.c" "CMakeFiles/test2.elf.dir/Core/Src/sysmem.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Core/Src/sysmem.c.obj.d"
+  "G:/project/proj_clion/test2/Core/Src/system_stm32f4xx.c" "CMakeFiles/test2.elf.dir/Core/Src/system_stm32f4xx.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Core/Src/system_stm32f4xx.c.obj.d"
+  "G:/project/proj_clion/test2/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c" "CMakeFiles/test2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj.d"
+  "G:/project/proj_clion/test2/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c" "CMakeFiles/test2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj.d"
+  "G:/project/proj_clion/test2/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c" "CMakeFiles/test2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj.d"
+  "G:/project/proj_clion/test2/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c" "CMakeFiles/test2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj.d"
+  "G:/project/proj_clion/test2/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c" "CMakeFiles/test2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj.d"
+  "G:/project/proj_clion/test2/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c" "CMakeFiles/test2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj.d"
+  "G:/project/proj_clion/test2/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c" "CMakeFiles/test2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj.d"
+  "G:/project/proj_clion/test2/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c" "CMakeFiles/test2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj.d"
+  "G:/project/proj_clion/test2/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c" "CMakeFiles/test2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj.d"
+  "G:/project/proj_clion/test2/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c" "CMakeFiles/test2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj.d"
+  "G:/project/proj_clion/test2/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c" "CMakeFiles/test2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj.d"
+  "G:/project/proj_clion/test2/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c" "CMakeFiles/test2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj.d"
+  "G:/project/proj_clion/test2/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c" "CMakeFiles/test2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj.d"
+  "G:/project/proj_clion/test2/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c" "CMakeFiles/test2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj.d"
+  "G:/project/proj_clion/test2/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c" "CMakeFiles/test2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj.d"
+  "G:/project/proj_clion/test2/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c" "CMakeFiles/test2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c.obj.d"
+  "G:/project/proj_clion/test2/Middlewares/Third_Party/RealThread_RTOS/components/device/device.c" "CMakeFiles/test2.elf.dir/Middlewares/Third_Party/RealThread_RTOS/components/device/device.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Middlewares/Third_Party/RealThread_RTOS/components/device/device.c.obj.d"
+  "G:/project/proj_clion/test2/Middlewares/Third_Party/RealThread_RTOS/components/finsh/cmd.c" "CMakeFiles/test2.elf.dir/Middlewares/Third_Party/RealThread_RTOS/components/finsh/cmd.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Middlewares/Third_Party/RealThread_RTOS/components/finsh/cmd.c.obj.d"
+  "G:/project/proj_clion/test2/Middlewares/Third_Party/RealThread_RTOS/components/finsh/finsh_port.c" "CMakeFiles/test2.elf.dir/Middlewares/Third_Party/RealThread_RTOS/components/finsh/finsh_port.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Middlewares/Third_Party/RealThread_RTOS/components/finsh/finsh_port.c.obj.d"
+  "G:/project/proj_clion/test2/Middlewares/Third_Party/RealThread_RTOS/components/finsh/msh.c" "CMakeFiles/test2.elf.dir/Middlewares/Third_Party/RealThread_RTOS/components/finsh/msh.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Middlewares/Third_Party/RealThread_RTOS/components/finsh/msh.c.obj.d"
+  "G:/project/proj_clion/test2/Middlewares/Third_Party/RealThread_RTOS/components/finsh/shell.c" "CMakeFiles/test2.elf.dir/Middlewares/Third_Party/RealThread_RTOS/components/finsh/shell.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Middlewares/Third_Party/RealThread_RTOS/components/finsh/shell.c.obj.d"
+  "G:/project/proj_clion/test2/Middlewares/Third_Party/RealThread_RTOS/libcpu/arm/cortex-m4/cpuport.c" "CMakeFiles/test2.elf.dir/Middlewares/Third_Party/RealThread_RTOS/libcpu/arm/cortex-m4/cpuport.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Middlewares/Third_Party/RealThread_RTOS/libcpu/arm/cortex-m4/cpuport.c.obj.d"
+  "G:/project/proj_clion/test2/Middlewares/Third_Party/RealThread_RTOS/src/clock.c" "CMakeFiles/test2.elf.dir/Middlewares/Third_Party/RealThread_RTOS/src/clock.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Middlewares/Third_Party/RealThread_RTOS/src/clock.c.obj.d"
+  "G:/project/proj_clion/test2/Middlewares/Third_Party/RealThread_RTOS/src/components.c" "CMakeFiles/test2.elf.dir/Middlewares/Third_Party/RealThread_RTOS/src/components.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Middlewares/Third_Party/RealThread_RTOS/src/components.c.obj.d"
+  "G:/project/proj_clion/test2/Middlewares/Third_Party/RealThread_RTOS/src/cpu.c" "CMakeFiles/test2.elf.dir/Middlewares/Third_Party/RealThread_RTOS/src/cpu.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Middlewares/Third_Party/RealThread_RTOS/src/cpu.c.obj.d"
+  "G:/project/proj_clion/test2/Middlewares/Third_Party/RealThread_RTOS/src/idle.c" "CMakeFiles/test2.elf.dir/Middlewares/Third_Party/RealThread_RTOS/src/idle.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Middlewares/Third_Party/RealThread_RTOS/src/idle.c.obj.d"
+  "G:/project/proj_clion/test2/Middlewares/Third_Party/RealThread_RTOS/src/ipc.c" "CMakeFiles/test2.elf.dir/Middlewares/Third_Party/RealThread_RTOS/src/ipc.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Middlewares/Third_Party/RealThread_RTOS/src/ipc.c.obj.d"
+  "G:/project/proj_clion/test2/Middlewares/Third_Party/RealThread_RTOS/src/irq.c" "CMakeFiles/test2.elf.dir/Middlewares/Third_Party/RealThread_RTOS/src/irq.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Middlewares/Third_Party/RealThread_RTOS/src/irq.c.obj.d"
+  "G:/project/proj_clion/test2/Middlewares/Third_Party/RealThread_RTOS/src/kservice.c" "CMakeFiles/test2.elf.dir/Middlewares/Third_Party/RealThread_RTOS/src/kservice.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Middlewares/Third_Party/RealThread_RTOS/src/kservice.c.obj.d"
+  "G:/project/proj_clion/test2/Middlewares/Third_Party/RealThread_RTOS/src/mem.c" "CMakeFiles/test2.elf.dir/Middlewares/Third_Party/RealThread_RTOS/src/mem.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Middlewares/Third_Party/RealThread_RTOS/src/mem.c.obj.d"
+  "G:/project/proj_clion/test2/Middlewares/Third_Party/RealThread_RTOS/src/memheap.c" "CMakeFiles/test2.elf.dir/Middlewares/Third_Party/RealThread_RTOS/src/memheap.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Middlewares/Third_Party/RealThread_RTOS/src/memheap.c.obj.d"
+  "G:/project/proj_clion/test2/Middlewares/Third_Party/RealThread_RTOS/src/mempool.c" "CMakeFiles/test2.elf.dir/Middlewares/Third_Party/RealThread_RTOS/src/mempool.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Middlewares/Third_Party/RealThread_RTOS/src/mempool.c.obj.d"
+  "G:/project/proj_clion/test2/Middlewares/Third_Party/RealThread_RTOS/src/object.c" "CMakeFiles/test2.elf.dir/Middlewares/Third_Party/RealThread_RTOS/src/object.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Middlewares/Third_Party/RealThread_RTOS/src/object.c.obj.d"
+  "G:/project/proj_clion/test2/Middlewares/Third_Party/RealThread_RTOS/src/scheduler.c" "CMakeFiles/test2.elf.dir/Middlewares/Third_Party/RealThread_RTOS/src/scheduler.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Middlewares/Third_Party/RealThread_RTOS/src/scheduler.c.obj.d"
+  "G:/project/proj_clion/test2/Middlewares/Third_Party/RealThread_RTOS/src/slab.c" "CMakeFiles/test2.elf.dir/Middlewares/Third_Party/RealThread_RTOS/src/slab.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Middlewares/Third_Party/RealThread_RTOS/src/slab.c.obj.d"
+  "G:/project/proj_clion/test2/Middlewares/Third_Party/RealThread_RTOS/src/thread.c" "CMakeFiles/test2.elf.dir/Middlewares/Third_Party/RealThread_RTOS/src/thread.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Middlewares/Third_Party/RealThread_RTOS/src/thread.c.obj.d"
+  "G:/project/proj_clion/test2/Middlewares/Third_Party/RealThread_RTOS/src/timer.c" "CMakeFiles/test2.elf.dir/Middlewares/Third_Party/RealThread_RTOS/src/timer.c.obj" "gcc" "CMakeFiles/test2.elf.dir/Middlewares/Third_Party/RealThread_RTOS/src/timer.c.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
